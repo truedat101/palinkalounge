be8241b0d7e83ad57ce2ed673ade76843b3a6933
David J. Kordsmeier <dkords@gmail.com>
2018-08-18 20:11:00 -0700

    Final changes to Hundroid school

--- hundroidschool/mySketch.js
+++ hundroidschool/mySketch.js
@@ -1,10 +1,11 @@
-var version = "v1.0.1";
+var version = "v1.0.2";
 var isOverRectangle = false;
 var x = 1;
-var y = 660;
+// These defaults get tossed
+var y = 660; 
 var w = 150;
 var h = 80;
-var x2 = 1170;
+var x2 = 1470;
 var a;
 var flagimg;
 var headlines = [
@@ -58,7 +59,7 @@ function setup() {
   // minim = new Minim(this);
 	x = width*0.009
   y = height*0.812;
-	x2 = width*0.7
+	x2 = width*0.8
 	w = width * 0.1
 	h =  height * 0.12
 }
@@ -70,17 +71,19 @@ function draw() {
   rect(x, y, w, h);  // 
   rect(x2, y, w, h); // Play
 	
-	// Play box
-	//rect(width*0.009, height*0.812, width * 0.1, height * 0.12);
   fill(0);
+  
+  // Play
   triangle(width*0.04, height*0.90421456, width*0.06875, height*0.8761175, width*0.04, height*0.8403576);
-  triangle(width*0.759375, height*0.90421456, width*0.7875, height*0.8761175, width*0.759375, height*0.8403576);
-  triangle(width*0.728125, height*0.90421456, width*0.75625, height*0.8761175, width*0.728125, height*0.8403576);
+  
+  // FFWD
+  triangle(width*0.859375, height*0.90421456, width*0.8875, height*0.8761175, width*0.859375, height*0.8403576);
+  triangle(width*0.828125, height*0.90421456, width*0.85625, height*0.8761175, width*0.828125, height*0.8403576);
   textSize(60);
   text(headlines[index],width*0.5, height*0.12771392); 
   line(0, height*0.19157088, width, height*0.19157088);
   textSize(20);
-  text("Hundroid School " + version +": learn important hungarian phrases. click play on the left to play sound and learn the phrase.",width*0.48, height*0.25542784);
+  text("Hundroid School " + version +":learn important hungarian words. Hit play on the left to play sound to hear the phrase.",width*0.48, height*0.25542784);
   text(" Click next on the right to continue to the next phrase", width*0.48, height*0.29);
 	text("By liamkords + truedat101 2018, Voices by Imi (Palinka Lounge)",width*0.48,height*0.38)
 	image(flagimg, width*0.28,height*0.48, flagimg.width/2, flagimg.height/2);
